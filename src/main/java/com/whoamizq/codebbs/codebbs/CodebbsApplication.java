862dd004bd2262c6d46cca582d6da1a2470be10f
iamZQ <1726891762@qq.com>
2020-02-06 21:11:14 +0800

    first commit init project

---
+++ src/main/java/com/whoamizq/codebbs/codebbs/CodebbsApplication.java
@@ -0,0 +1,13 @@
+package com.whoamizq.codebbs.codebbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodebbsApplication.class, args);
+    }
+
+}
