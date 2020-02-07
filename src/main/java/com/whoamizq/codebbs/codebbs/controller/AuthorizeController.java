20d58e66b4f4b2de841dee7076fd2bfbb753afe5
iamZQ <1726891762@qq.com>
2020-02-07 17:12:14 +0800

    用户数据插入

--- src/main/java/com/whoamizq/codebbs/codebbs/controller/AuthorizeController.java
+++ src/main/java/com/whoamizq/codebbs/codebbs/controller/AuthorizeController.java
@@ -2,11 +2,17 @@ package com.whoamizq.codebbs.codebbs.controller;
 
 import com.whoamizq.codebbs.codebbs.dto.AccessTokenDTO;
 import com.whoamizq.codebbs.codebbs.dto.GithubUser;
+import com.whoamizq.codebbs.codebbs.mapper.UserMapper;
+import com.whoamizq.codebbs.codebbs.model.User;
 import com.whoamizq.codebbs.codebbs.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /**
  * 认证登录，回调
@@ -15,19 +21,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("0d7350863c9200f3a014");
-        accessTokenDTO.setClient_secret("e9ebebf46d665cf982e0848fa98e3bff7bec5c41");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8081/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+//        System.out.println(user.getName());
+        if (githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
+            //登录成功，使用cookie和session
+            return "redirect:/";
+        }else {
+            //登录失败
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/com/whoamizq/codebbs/codebbs/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.whoamizq.codebbs.codebbs.mapper;
+
+import com.whoamizq.codebbs.codebbs.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
