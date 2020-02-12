5f3101b2c11f67ac80572864b07f17a525f239fb
iamZQ <1726891762@qq.com>
2020-02-12 23:01:17 +0800

    bug修复

--- src/main/java/com/whoamizq/codebbs/codebbs/provider/GithubProvider.java
+++ src/main/java/com/whoamizq/codebbs/codebbs/provider/GithubProvider.java
@@ -33,9 +33,12 @@ public class GithubProvider {
     public GithubUser getUser(String accessToken){
         OkHttpClient client = new OkHttpClient();
 
+//        Request request = new Request.Builder()
+//                .header("Authorization","token"+accessToken)
+//                .url("https://api.github.com/user")
+//                .build();
         Request request = new Request.Builder()
-                .url("https://api.github.com/user")
-                .header("Authorization","token "+accessToken)
+                .url("https://api.github.com/user?access_token=" + accessToken)
                 .build();
         try{
             Response response = client.newCall(request).execute();
