35cb76141ae97f49deaa0b17a5f7adabcaf97bad
iamZQ <1726891762@qq.com>
2020-02-12 22:03:37 +0800

    修改github官方推荐api登录方式

--- src/main/java/com/whoamizq/codebbs/codebbs/provider/GithubProvider.java
+++ src/main/java/com/whoamizq/codebbs/codebbs/provider/GithubProvider.java
@@ -34,7 +34,8 @@ public class GithubProvider {
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
-                .url("https://api.github.com/user?access_token=" + accessToken)
+                .url("https://api.github.com/user")
+                .header("Authorization","token "+accessToken)
                 .build();
         try{
             Response response = client.newCall(request).execute();
